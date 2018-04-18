baf22929476103120512cc8af608283a6eb194bf
Zulko <valentin.zulkower@gmail.com>
2018-04-18 18:40:47 +0100

    first commit

--- examples/example.py
+++
@@ -1,9 +0,0 @@
-"""Basic PDF report generation from a Pug template with pdf_reports.
-
-A HTML page is generated from a template and rendered as a local PDF file.
-"""
-
-from pdf_reports import pug_to_html, write_report
-
-html = pug_to_html("example_template.pug", title="My report")
-write_report(html, "example.pdf")
--- pdf_reports/__init__.py
+++
@@ -1,3 +0,0 @@
-from .pdf_reports import pug_to_html, write_report, GLOBALS
-from .tools import (dataframe_to_html, style_table_rows, add_css_class,
-                    figure_data, JupyterPDF)
